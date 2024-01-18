b9297d063292880fadf76c0a17a0e755272707ee
Vincent <aavincent@deloitte.com>
2024-01-18 17:23:57 +0530

    ReactDom.hydrate tried

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider} from 'react-redux';
-import configureStore from './store'
 
-import App from './App'; 
+import AppServer from './AppServer';
 import "./index.css";
 
-const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-        <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.hydrate(<AppServer />, document.getElementById('root'));
